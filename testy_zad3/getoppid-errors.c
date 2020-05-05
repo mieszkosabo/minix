b62a62b4e8a67ed85e1872e9becc32d985d17c52
mieszko <mieszko.mac@gmail.com>
2020-05-05 16:08:46 +0200

    test na errory getoppid

---
+++ testy_zad3/getoppid-errors.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+// można zmieniać aż do 30000, jeśli dla małych wartości jest ok
+#define CHECKRANGE 20
+
+int main(void) {
+    // pids range from 0 to 30k - 1
+    pid_t invalid_pid = 30000;
+    if (getoppid(invalid_pid) != 0) {
+        printf("powinno się wypisać INVALID ARGUMENT albo coś podobnego\n");
+        printf("getoppid error: %s\n", strerror(errno));
+    }
+
+    printf("Nic po tej linijce nie powinno się wypisać\n");
+    for (int i = 0; i <= CHECKRANGE; i++) {
+        pid_t res = getoppid(i);
+        if (res >= 30000 || res < 0) {
+            printf("przyps, prawdopodobnie wypisujesz ppid nieistniejącego procesu:\n");
+            printf("pid: %d oppid: %u\n\n", i, getoppid(i));
+        }
+    }
+
+    return 0;
+}
